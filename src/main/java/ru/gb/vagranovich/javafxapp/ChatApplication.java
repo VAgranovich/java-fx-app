88bb3540e5acd2b9e4e9cf08d4e79aecde25c7f1
Vagranovich <vadim.agranovich@gmail.com>
2022-06-18 23:28:08 +0300

    Homework commit

--- src/main/java/ru/gb/vagranovich/javafxapp/ChatApplication.java
+++ src/main/java/ru/gb/vagranovich/javafxapp/ChatApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class BullAndCowApplication extends Application {
+public class ChatApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BullAndCowApplication.class.getResource("main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ChatApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Чат");
         stage.setScene(scene);
         stage.show();
     }
